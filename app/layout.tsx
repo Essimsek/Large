55b5d6584664c7ddc125971d23cc4bdac6deb927
emre simsek <emresimsek321@gmail.com>
2025-05-30 16:33:45 +0300

    AbhayaLibre fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,32 @@
 import type { Metadata } from "next";
+import localFont from 'next/font/local'
 import "./globals.css";
+
+const abhayaliFont = localFont({
+  src: [
+    {
+      path: './fonts/AbhayaLibre-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/AbhayaLibre-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './fonts/AbhayaLibre-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/AbhayaLibre-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    }
+  ],
+  variable: '--abhayali-font',
+});
 
 export const metadata: Metadata = {
   title: "Large",
@@ -13,8 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
+      <body className={`font-sans ${abhayaliFont.variable}`}>
         {children}
       </body>
     </html>
