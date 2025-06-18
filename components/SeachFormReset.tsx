aec858427fc65483eeff38e001e70e831d768df5
emre simsek <emresimsek321@gmail.com>
2025-06-18 17:52:25 +0300

    SearchFormReset bug fix

--- components/SeachFormReset.tsx
+++ components/SeachFormReset.tsx
@@ -5,8 +5,16 @@ import { Button } from './ui/button';
 import { X } from 'lucide-react';
 
 const SeachFormReset = () => {
+
+    const handleReset = () => {
+        const searchForm = document.querySelector('form[name="search"]') as HTMLFormElement;
+        if (searchForm) {
+            searchForm.reset();
+        }
+    };
+
     return (
-        <Button type="reset" asChild className='absolute right-11 top-1/2 -translate-1/2 bg-gray-200 hover:bg-red-400 text-gray-500 hover:text-gray-700 p-1 rounded-full transition-colors duration-150'>
+        <Button type="reset" onClick={handleReset} asChild className='absolute right-11 top-1/2 -translate-1/2 bg-gray-200 hover:bg-red-400 text-gray-500 hover:text-gray-700 p-1 rounded-full transition-colors duration-150'>
             <Link href="/" className="p-1.5">
                 <X />
             </Link>
