75c49e14d23bede9df5e01e4b4ffd41e12e0586b
emre simsek <emresimsek321@gmail.com>
2025-06-30 18:53:55 +0300

    add author type and notFoundPage add

--- sanity/types.ts
+++ sanity/types.ts
@@ -1,12 +1,18 @@
 
+export type authorType = {
+  _id: string;
+  username: string;
+  image?: string;
+}
+
 export type startupCardType = {
   _createdAt: Date | string;
   _id: number;
-  author: { _id: string, username: string, image?: string },
+  author: authorType;
   views: number;
   likes: number;
   description: string;
-  image: string;
+  image?: string;
   slug?: string;
   category: string;
   title: string;
