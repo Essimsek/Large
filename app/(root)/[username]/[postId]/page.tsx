f12f66429338b41c496f355d06c5af79610c5afb
emre simsek <emresimsek321@gmail.com>
2025-07-04 19:54:12 +0300

    fetch post by slug and username

--- app/(root)/[username]/[postId]/page.tsx
+++ app/(root)/[username]/[postId]/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import { GET_POST_BY_SLUG_QUERY } from '@/sanity/lib/queries';
+import { client } from '@/sanity/lib/client';
+import { Post } from '@/sanity.types';
 
 const Page = async ({params}: {
     params: Promise<{username: string, postId: string}>
 }) => {
     const {username, postId} = await params;
+    const currentPost = await client.fetch(GET_POST_BY_SLUG_QUERY, {username, slug: postId}
+    ) as Post;
+
+    const {title, description, category, image, likes, views, _createdAt, _updatedAt, pitch} = currentPost
     return (
-        <div>
-            Hello From startup {username} {postId}
-        </div>
+        <>
+            <div>
+                Hello From startup {title}
+                <p>{description}</p>
+                <p>Category: {category}</p>
+            </div>
+        </>
     );
 }
 
