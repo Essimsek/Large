fdafa4ef7ce4b4ce8c6cf84fc00187cc4893bdd7
Essimsek <emresimsek321@gmail.com>
2025-07-16 18:29:18 +0300

    user pages

--- app/(root)/[username]/page.tsx
+++ app/(root)/[username]/page.tsx
@@ -4,22 +4,27 @@ import { GET_USER_BY_USERNAME_QUERY } from '@/sanity/lib/queries'
 import { Author } from '@/sanity.types';
 import { client } from '@/sanity/lib/client';
 import { notFound } from 'next/navigation';
-import { auth } from '@/auth';
-
+import Header from '@/components/Header';
+import Image from 'next/image';
 async function Page({ params }:
     { params: Promise<{username: string}> }
 ) {
-    const session = await auth();
     const { username } = await params;
     const user = await client.fetch(GET_USER_BY_USERNAME_QUERY, {username}) as Author;
-    console.log("User:", user);
     if (!user) {
         notFound();
     }
+    const userImage = user?.image || "";
     return (
-        <>
-            <h1>User Profile Page {username}</h1>
-        </>
+        <div className="red-container pattern">
+            <Image src={userImage} width={200} height={200} className='rounded-full' alt={"Profile photo"} />
+            <div className="w-full flex flex-col items-center justify-center">
+                <Header title={user.name || ""} />
+                <p className="font-semibold text-white text-center mt-4">
+                    {user.bio || "This user has not set up their profile yet."}
+                </p>
+            </div>
+        </div>
     )
 }
 
