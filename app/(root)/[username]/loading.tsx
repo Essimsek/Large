ace636a83a62b3aad6b38062a5521399f5670de8
Essimsek <emresimsek321@gmail.com>
2025-08-03 17:34:13 +0300

    Loading pages for username and post page

---
+++ app/(root)/[username]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-xl font-semibold text-gray-600 animate-pulse">
+        Loading user profile and posts...
+      </p>
+    </div>
+  );
+}
